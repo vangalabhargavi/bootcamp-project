df57b0cb6e563fa13f773839664174ab74151268
vangalabhargavi <128502397+vangalabhargavi@users.noreply.github.com>
2023-05-18 16:48:28 +0530

    uploaded the project file

---
+++ bootcamp project.py
@@ -0,0 +1,31 @@
+import random
+name = input("What is your name? ")
+print("Good luck !",name)
+words = ['apple', 'mango', 'grapes', 'orange',
+         'banana', 'strawberry', 'pineapple', 'guava',
+         'cherry', 'sapota', 'watermelon', 'custardapple']
+word = random.choice(words)
+print("Guess the characters")
+guesses = ''
+turns = 12
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_")
+            failed += 1
+    if failed == 0:
+        print("You Win")
+        print("The word is: ", word)
+        break
+    print()
+    guess = input("guess a character:")
+    guesses += guess
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have", + turns, 'more guesses')
+        if turns == 0:
+            print("You Loose")
